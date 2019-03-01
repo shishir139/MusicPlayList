7399adbc6f4103fa935b780f457f44c7c069ae6b
shishir139 <shishir139@gmail.com>
2019-02-28 21:52:30 -0600

    Add files via upload

---
+++ Album.java
@@ -0,0 +1,39 @@
+package com.company.shishir;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public boolean addSong(String title, double duration)
+    { if (findSong(title) == null) {
+        this.songs.add(new Song(title, duration));
+        return true;
+    }
+return false;
+
+    }
+
+    private Song findSong(String title) {
+        for(Song checkedSong: this.songs) {
+            if(checkedSong.getTitle().equals(title)) {
+                return checkedSong;
+            }
+        }
+        return null;
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+        int index = trackNumber - 1;
+        if(index > 0 ) && (index)
+    }
+}
